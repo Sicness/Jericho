a2e4ef1ab3ed03b33f0fbe77843b2550159d9255
Anton Balahsov <anton.balashov@kohls.com>
2014-06-28 14:22:53 +0400

    Robust IT

--- integration-tests/integration-tests.sh
+++ integration-tests/integration-tests.sh
@@ -15,18 +15,25 @@ function cleanup
   [ -f $MONITOR_LOG ] && rm -f $MONITOR_LOG
 }
 
-echo "Trying to get "IT" msg via gwSocket -> pipeline -> monitor"
+echo ""
+echo '==> Trying to get "IT" msg via gwSocket -> pipeline -> monitor'
 
 java -jar $(getJarPath pipeline) &
 java -jar $(getJarPath gwSocket) &
 java -jar $(getJarPath monitor) > $MONITOR_LOG &
-sleep 1
 
-echo "IT" | nc -v 127.0.0.1 10001
-for i in {1..3}; do
-  if grep -q IT $MONITOR_LOG; then echo "SUCCESS"; cleanup; exit 0; fi
+for i in {1..5}; do
+  sleep 1
+  if echo "IT" | nc -v 127.0.0.1 10001; then break; fi
+  if [ $i == 5 ]; then echo "FAILED: can't connect to gwSocket"; cleanup; exit 3; fi
 done
 
-echo "FAILED"
+for i in {1..3}; do
+  if grep -q IT $MONITOR_LOG; then echo "SUCCESS"; cleanup; exit 0; fi
+  sleep 1
+done
+
+echo "FAILED. Logs of $MONITOR_LOG:"
+cat $MONITOR_LOG
 cleanup
 exit 2
